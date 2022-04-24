398ef9c82c4322f0b81a3210f8ab06c51fc758e8
batool-22 <rose.sy2001.tu@gmail.com>
2022-04-24 04:36:33 +0300

    Add board

--- components/CreateProject.js
+++ components/CreateProject.js
@@ -21,10 +21,7 @@ import Field from './Field'
 import SectionTitle from './Form/SectionTitle';
 import BoardTiltle from './Form/BoardTiltle';
 import MemberTitle from './Form/MemberTitle';
-import { boardState, boardType, boardTypeState } from "../atoms/boardAtoms";
-import { AnimatePresence } from "framer-motion";
-import BoardForm from './BoardForm';
-import Modal from './Modal';
+
 
 
 function CreateProject() {
@@ -33,6 +30,9 @@ function CreateProject() {
     const [projectTitle, setprojectTitle] = useState("");
     const [projectDate, setprojectDate] = useState("");
     const [loading, setLoading] = useState(false);
+    const [boardTitle, setboardTitle] = useState("");
+    const [boardType, setboardType] = useState("");
+    const [open, setIsOpen] = useState(false);
 
     const sendProject = async () => {
         if (loading) return;
@@ -45,6 +45,8 @@ function CreateProject() {
             title: projectTitle,
             date: projectDate,
             details: projectDetails,
+            boardtitle: boardTitle,
+            type:boardType,
             timestamp: serverTimestamp(),
         });
 
@@ -52,13 +54,11 @@ function CreateProject() {
         setprojectTitle("");
         setprojectDate("");
         setProjectDetails("");
+        setboardTitle("");
+        setboardType("");
     };
 
-
-
-    //const [boardOpen, setboardOpen] = useRecoilState(boardState)
-    //const [boardType, setboardType] = useRecoilState(boardTypeState)
-
+    
     return (
         <div className="space-y-4 divide-y divide-black-300">
             <div className="flex space-x-4 divide-x divide-black-300">
@@ -101,37 +101,62 @@ function CreateProject() {
                 <div className=" justify-between items-center overflow-auto w-1/3 divide-black-300 p-4 space-y-4">
                     <BoardTiltle iconn={<MdOutlineSpaceDashboard className="w-5 h-5 text-white" />} textt="Boards" />
 
-                    <button className='flex items-center cursor-pointer text-gray-100'
-                    > Add </button>
+                 {open ? (
+                <div class="w-full max-w-lg p-5">
+                            <form class="bg-black-100 flex justify-center shadow-xl rounded px-8 pt-6 pb-8 ">
+                                    <div className="w-full space-y-5 ">
+                                        <div className="space-y-3"> 
+                                        <div class="flex items-center justify-between">
+                                        <h2 className='text-white font-bold'>Add Board</h2>
+                                        <button class="bg-primary text-white rounded px-4 py-1.5 font-bold shadow-md hover:bg-primary-dark disabled:hover:bg-black-300 disabled:opacity-50 disabled:cursor-default" type="button"
+                                       onClick={sendProject} >Save</button>
+                                        
+                                </div>
+                                        <Field
+                                          fieldValue={boardTitle}
+                                          fieldFunc={(e) => setboardTitle(e.target.value)}
+                                          fieldType="text"
+                                          fieldId=" board Title"
+                                          title="Title"
+                                         placeHolder="Ex: today todos checklist"
+                                            />
+                                      </div>
+                                <div class="flex items-center justify-between ">
+                                <label  className='text-gray-100'>Methdology : </label>
+                                    <select class="bg-black-100 appearance-none w-32 text-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                                  value={boardType}
+                                  onChange={(e) => setboardType(e.target.value)}
+                                  >
+                                            <option>Scrum </option>
+                                            <option>WaterFull</option>
+                                            <option>Agile</option>
+                                    </select>
+                                </div>
+                             </div>
+                         </form>
+                     </div>
+                 ) : (
+                    <button className='flex items-center cursor-pointer text-gray-100 '
+                    onClick={() => setIsOpen(true)} > Add </button>
+                 )}
+                     
                 </div>
 
 
                 {/*Members section*/}
                 <div className="w-1/3 space-y-8 p-4">
                     <MemberTitle icone={<BsFillPersonLinesFill className="w-5 h-5 text-white" />} texte="Members" />
-
                 </div>
             </div>
-
 
             <div className="pt-4">
                 <button
                     className="bg-primary text-white rounded px-4 py-1.5 font-bold shadow-md hover:bg-primary-dark disabled:hover:bg-black-300 disabled:opacity-50 disabled:cursor-default"
                     disabled={!projectDetails.trim()}
-                    onClick={sendProject}
-
-                >
+                    onClick={sendProject} >
                     Save project
                 </button>
             </div>
-
-            {/*{
-          <AnimatePresence>
-            {boardOpen && (
-              <Modal handleClose={() => setboardOpen(false)} type={boardType} comp={<BoardForm />}/>
-            )}
-          </AnimatePresence>
-        }*/}
 
         </div>
 
