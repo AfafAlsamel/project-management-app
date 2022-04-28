0b69efd6dcb8b87ff3ede62137c3d505b7d5e7d1
batool-22 <rose.sy2001.tu@gmail.com>
2022-04-28 05:53:26 +0300

    add many board

--- components/CreateProject.js
+++ components/CreateProject.js
@@ -32,21 +32,23 @@ function CreateProject() {
     const [loading, setLoading] = useState(false);
     const [boardTitle, setboardTitle] = useState("");
     const [boardType, setboardType] = useState("");
-    const [open, setIsOpen] = useState(false);
+    const handleSelect=(e)=>{
+
+        console.log(e);
+    
+        setValue(e)
+    
+      }
 
     const sendProject = async () => {
         if (loading) return;
         setLoading(true);
-        const docRef = await addDoc(collection(db, "Projects"), {
-            // id: session.user.uid,
-            // username: session.user.name,
-            // userImg: session.user.image,
-            // tag: session.user.tag,
+        const docRef = await addDoc(collection( db ,"Project"), {
             title: projectTitle,
             date: projectDate,
             details: projectDetails,
-            boardtitle: boardTitle,
-            type:boardType,
+            //boardtitle: boardTitle,
+            //type:boardType,
             timestamp: serverTimestamp(),
         });
 
@@ -54,9 +56,48 @@ function CreateProject() {
         setprojectTitle("");
         setprojectDate("");
         setProjectDetails("");
-        setboardTitle("");
-        setboardType("");
+        //setboardTitle("");
+        //setboardType("");
     };
+
+    {/*const sendBoard = async () => {
+        if (loading) return;
+        setLoading(true);
+        const docRef = await addDoc(collection(db, "Boards"), {
+            boardtitle: boardTitle,
+            type:boardType,
+            timestamp: serverTimestamp(),
+        });
+
+        setLoading(false);
+        setboardFields([...boardFields, object])
+    };
+*/}
+    const [boardFields, setboardFields] = useState([
+        { title: '', select: '' },
+      ])
+    
+      const handleFormChange = (event, index) => {
+        let data = [...boardFields];
+        data[index][event.target.name] = event.target.value;
+        setboardFields(data);
+      }
+    
+      const submit = (e) => {
+        e.preventDefault();
+        console.log(boardFields)
+      }
+    
+      const addBoard = () => {
+        let object = {
+          title: '',
+          select: ''
+        }
+    
+        setboardFields([...boardFields, object])
+      }
+    
+     
 
     
     return (
@@ -101,44 +142,53 @@ function CreateProject() {
                 <div className=" justify-between items-center overflow-auto w-1/3 divide-black-300 p-4 space-y-4">
                     <BoardTiltle iconn={<MdOutlineSpaceDashboard className="w-5 h-5 text-white" />} textt="Boards" />
 
-                 {open ? (
-                <div class="w-full max-w-lg p-5">
-                            <form class="bg-black-100 flex justify-center shadow-xl rounded px-8 pt-6 pb-8 ">
-                                    <div className="w-full space-y-5 ">
+                    <button className='flex items-center cursor-pointer text-gray-100 '
+                    onClick={addBoard} > Add </button>
+
+                     <div class="w-full max-w-lg p-5">
+                            <form  onSubmit={submit} class="bg-black-100 flex justify-center shadow-xl rounded px-8 pt-6 pb-8 ">
+                            {boardFields.map((form , index) => {
+                                 return (
+                                    <div className="w-full space-y-5 " key={index}>
                                         <div className="space-y-3"> 
                                         <div class="flex items-center justify-between">
                                         <h2 className='text-white font-bold'>Add Board</h2>
                                         <button class="bg-primary text-white rounded px-4 py-1.5 font-bold shadow-md hover:bg-primary-dark disabled:hover:bg-black-300 disabled:opacity-50 disabled:cursor-default" type="button"
-                                       onClick={sendProject} >Save</button>
+                                       onClick={submit} >Save</button>
                                         
                                 </div>
                                         <Field
-                                          fieldValue={boardTitle}
-                                          fieldFunc={(e) => setboardTitle(e.target.value)}
+                                          fieldValue={form.name}
+                                          fieldFunc={event => handleFormChange(event, index)}
                                           fieldType="text"
                                           fieldId=" board Title"
                                           title="Title"
+                                          name="title"
                                          placeHolder="Ex: today todos checklist"
                                             />
                                       </div>
                                 <div class="flex items-center justify-between ">
                                 <label  className='text-gray-100'>Methdology : </label>
-                                    <select class="bg-black-100 appearance-none w-32 text-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-                                  value={boardType}
-                                  onChange={(e) => setboardType(e.target.value)}
+                                <select class="bg-black-100 appearance-none w-32 text-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                                  value={form.select}
+                                  onChange={event => handleFormChange(event, index)}
+                                  name="select"
+
                                   >
                                             <option>Scrum </option>
                                             <option>WaterFull</option>
                                             <option>Agile</option>
                                     </select>
                                 </div>
+                 
                              </div>
+                                 )
+                            })}
                          </form>
+            
                      </div>
-                 ) : (
-                    <button className='flex items-center cursor-pointer text-gray-100 '
-                    onClick={() => setIsOpen(true)} > Add </button>
-                 )}
+            
+
                      
                 </div>
 
